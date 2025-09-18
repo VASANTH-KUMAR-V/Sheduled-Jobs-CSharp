18d608855a2868fcaeb771b8a0045bf57689b483
VASANTH-KUMAR-V <vasanthvc02@gmail.com>
2025-09-18 12:18:48 +0530

    Add project files.

---
+++ Sheduled Jobs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sheduled_Jobs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
